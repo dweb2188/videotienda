f161a095992c06e637fcddff0fe0467a87ad2218
horaciokar <horaciokar@gmail.com>
2019-05-14 21:37:23 -0500

    Version 1.1

--- videotienda.sql
+++ videotienda.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.5.24-log)
-# Date: 2019-05-12 21:55:08
+# Date: 2019-05-14 21:36:53
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -16,7 +16,7 @@ CREATE TABLE `clientes` (
   `telefono` varchar(255) DEFAULT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "clientes"
@@ -54,13 +54,13 @@ CREATE TABLE `peliculas` (
   `sinopsis` longtext,
   `estado` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "peliculas"
 #
 
-INSERT INTO `peliculas` VALUES (1,'Star Wars','Star Wars es una franquicia compuesta primordialmente de una serie de películas concebidas por el cineasta estadounidense George Lucas, y producidas y distribuidas por The Walt Disney Company a partir de 2012. Su trama describe las vivencias de un grupo de personajes que habitan en una galaxia ficticia e interactúan con elementos como la Fuerza, un campo de energía metafísico y omnipresente4? que posee un «lado oscuro» provocado por la ira, el miedo y el odio.',0),(2,'Avengers','fgfgfgf',0),(3,'Batman','fdfdfdfd',1),(4,'Iron Man','rrere',1);
+INSERT INTO `peliculas` VALUES (1,'Star Wars',' Su trama describe las vivencias de un grupo de personajes que habitan en una galaxia ficticia e interactúan con elementos como la Fuerza, un campo de energía metafísico y omnipresente4? que posee un «lado oscuro» provocado por la ira, el miedo y el odio.',0),(2,'Avengers','El director de la Agencia SHIELD decide reclutar a un equipo para salvar al mundo de un desastre casi seguro cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial.',0),(3,'Batman','Después de la muerte de sus padres, el joven heredero Bruce Wayne se convierte en un vengador enmascarado que lucha contra las fuerzas del mal en Ciudad Gótica.',1),(4,'Iron Man','Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.',1);
 
 #
 # Structure for table "tiendas"
@@ -78,3 +78,4 @@ CREATE TABLE `tiendas` (
 # Data for table "tiendas"
 #
 
+INSERT INTO `tiendas` VALUES (1,'El Poblado','Carrera 32'),(2,'Laureles','Parque Laureles'),(3,'Belen','Calle1');
