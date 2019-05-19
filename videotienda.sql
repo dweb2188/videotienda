7f3ec7736128a6523d5881feeca95f362d62f24a
horaciokar <horaciokar@gmail.com>
2019-05-18 19:21:24 -0500

    Version 1.1

--- videotienda.sql
+++ videotienda.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.5.24-log)
-# Date: 2019-05-16 23:31:53
+# Date: 2019-05-18 19:21:06
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -15,6 +15,7 @@ CREATE TABLE `clientes` (
   `documento` varchar(255) DEFAULT NULL,
   `telefono` varchar(255) DEFAULT NULL,
   `direccion` varchar(255) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
@@ -22,7 +23,7 @@ CREATE TABLE `clientes` (
 # Data for table "clientes"
 #
 
-INSERT INTO `clientes` VALUES (1,'Horacio','Karzon','452168','3012186736','Carrera32'),(2,'Juan','David','4343434','43434343','Calle10'),(3,'pepe','sasasa','3232','32323232','morere'),(4,'pablo','pepepe','324646','55755555','aaaa');
+INSERT INTO `clientes` VALUES (1,'Horacio','Karzon','452168','3012186736','Carrera32',0),(2,'Juan','David','4343434','43434343','Calle10',0),(3,'pepe','sasasa','3232','32323232','morere',NULL),(4,'pablo','pepepe','324646','55755555','aaaa',NULL);
 
 #
 # Structure for table "metodo_pago"
@@ -54,6 +55,7 @@ CREATE TABLE `peliculas` (
   `sinopsis` longtext,
   `estado` tinyint(1) DEFAULT NULL,
   `cliente` varchar(255) DEFAULT NULL,
+  `cant` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
@@ -61,7 +63,7 @@ CREATE TABLE `peliculas` (
 # Data for table "peliculas"
 #
 
-INSERT INTO `peliculas` VALUES (1,'Star Wars',' Su trama describe las vivencias de un grupo de personajes que habitan en una galaxia ficticia e interactúan con elementos como la Fuerza, un campo de energía metafísico y omnipresente4? que posee un «lado oscuro» provocado por la ira, el miedo y el odio.',1,NULL),(2,'Avengers','El director de la Agencia SHIELD decide reclutar a un equipo para salvar al mundo de un desastre casi seguro cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial.',0,NULL),(3,'Batman','Después de la muerte de sus padres, el joven heredero Bruce Wayne se convierte en un vengador enmascarado que lucha contra las fuerzas del mal en Ciudad Gótica.',1,'horacio'),(4,'Iron Man','Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.',1,'horacio');
+INSERT INTO `peliculas` VALUES (1,'Star Wars',' Su trama describe las vivencias de un grupo de personajes que habitan en una galaxia ficticia e interactúan con elementos como la Fuerza, un campo de energía metafísico y omnipresente4? que posee un «lado oscuro» provocado por la ira, el miedo y el odio.',0,'',0),(2,'Avengers','El director de la Agencia SHIELD decide reclutar a un equipo para salvar al mundo de un desastre casi seguro cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial.',0,'',0),(3,'Batman','Después de la muerte de sus padres, el joven heredero Bruce Wayne se convierte en un vengador enmascarado que lucha contra las fuerzas del mal en Ciudad Gótica.',0,'',0),(4,'Iron Man','Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.',0,'',0);
 
 #
 # Structure for table "tiendas"
